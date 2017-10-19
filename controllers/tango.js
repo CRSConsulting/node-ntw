8983071e1325bde5760305de015f25e68958102c
CRS <crs@His-Name-is-Jerry-Maxey.local>
2017-10-19 11:55:25 -0700

    added getWinner in mobile.js, added tango controller, fixed node-rest-client post method for tangoAPI

--- controllers/tango.js
+++ controllers/tango.js
@@ -1,36 +1,52 @@
-const options = {
-  user: process.env.TANGO_USER,
-  password: process.env.TANGO_PASSWORD,
-};
+const Client = require('node-rest-client').Client;
 
-const client = require('node-rest-client-promise').Client(options);
 
-const args = {
-  data: {
-    accountIdentifier: 'ntw-one',
-    amount: 10,
-    customerIdentifier: 'test-customer',
-    emailSubject: 'New Order',
-    message: 'testing',
-    recipient: {
-      email: 'john.crs.consulting@gmail.com',
-      firstName: '1pm Tuesday',
-      lastName: 'testing',
+exports.insertTango = (req, res) => {
+  const optionsAuth = {
+    user: process.env.TANGO_USER,
+    password: process.env.TANGO_PASSWORD,
+  };
+  const client = new Client(optionsAuth);
+  const args = {
+    data: {
+      accountIdentifier: 'ntw-one',
+      amount: 10,
+      customerIdentifier: 'test-customer',
+      emailSubject: 'New Order',
+      message: 'testing',
+      recipient: {
+        email: 'john.crs.consulting@gmail.com',
+        firstName: '11:41 am',
+        lastName: 'testing',
+      },
+      sendEmail: true,
+      sender: {
+        firstName: 'John',
+        lastName: 'Yu',
+      },
+      utid: 'U561593',
     },
-    sendEmail: true,
-    sender: {
-      firstName: 'John',
-      lastName: 'Yu',
+    headers: {
+      'Content-Type': 'application/json',
     },
-    utid: 'U561593',
-  },
-  headers: {
-    'Content-Type': 'application/json',
-  },
-};
+  };
+  function handleResponse(data, response) {
+    if (response.statusCode === 201) {
+      res.json(data);
+    } else {
+      switch (response.statusCode) {
+        case 404:
+          res.status(404).send('Page not found.');
+          break;
 
-client.getPromise('https://integration-api.tangocard.com/raas/v2/orders', args).then((data) => {
-  console.log('data', data.data);
-}).catch((err) => {
-  console.log('err', err);
-});
+        case 500:
+          res.status(500).send('Internal server error.');
+          break;
+
+        default:
+          console.log(`Response status code: ${response.statusCode}`);
+      }
+    }
+  }
+  return client.post('https://integration-api.tangocard.com/raas/v2/orders', args, handleResponse);
+};
