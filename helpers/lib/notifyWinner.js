fd6c7a51d2723c59faca98de7c73a5d7fd10283e
CRSConsulting <danny@crs.consulting>
2017-12-15 18:16:05 -0800

    updated message in sendUserMessages

--- helpers/lib/notifyWinner.js
+++ helpers/lib/notifyWinner.js
@@ -22,9 +22,9 @@ const sendUserMessages = (sessionCall, winners, res) => {
   const winner = winners.winners[winners.winnerIndex];
   const body = JSON.parse(sessionCall[0].slice(867));
   const sessionToken = body.user.session_token;
-  // const phoneNumber = firstPlace.phone;
+  // const phoneNumber = winner.phone;
   const phoneNumber = 6178204019;
-  const message = 'Congrats you have won!';
+  const message = 'YOU WON!! Thank you for your submission to our Brave Works Sweepstakes. Next, you will be sent an email verification request to verify your email to ensure we have the correct address where to send your prize. Depending on your submission of all the information required by law to collect your prize, your prize will be awarded via email within 48 hours after your email address verification. CONGRATULATIONS!';
   const sendText = calls.delay(Math.random() * 10000).then(() =>
     calls.getAsync(`curl -v -D - -H 'Authorization: Token token="${sessionToken}", type="session"' -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"shortcode_string":"41444","phone_number":"${phoneNumber}","message":"${message}"' https://app.mobilecause.com/api/v2/messages/send_sms`))
     .catch((err) => {
